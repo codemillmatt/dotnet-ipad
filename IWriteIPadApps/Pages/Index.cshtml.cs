eb1ba2ac8b1bb3651ab91e42de025f2adf3811e3
Matt Soucoup <msoucoup@codemilltech.com>
2021-05-26 02:56:29 +0000

    adding all the files

---
+++ IWriteIPadApps/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace IWriteIPadApps.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        static HttpClient client;
+
+        public IEnumerable<string> ThingsICanDo { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+
+            client = new HttpClient();
+        }
+
+        public async Task OnGet()
+        {
+            var functionUrl = "https://codemillmatt-dotnet-ipad-3v7w-7071.githubpreview.dev/api/LetsGetFunky";
+
+            var thingsICanDoJson = await client.GetStringAsync(functionUrl);
+
+            ThingsICanDo = JsonConvert.DeserializeObject<string[]>(thingsICanDoJson);
+        }
+    }
+}
---
+++ functions/LetsGetFunky.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace IWriteIPadApps.Functions
+{
+    public static class LetsGetFunky
+    {
+        [FunctionName("LetsGetFunky")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string[] thingsICanDo = new string[] { 
+                "Write .NET apps on an IPAD!!", 
+                "Write Azure Functions on an IPAD!!", 
+                "Write blogs on an IPAD!!!" 
+            };
+
+            return new OkObjectResult(thingsICanDo);
+        }
+    }
+}
